6a8e7db19985cd696121eb8a60a5630ebb1d4a1e
trench009 <amennajam7@gmail.com>
2022-10-11 19:18:46 +0500

    add

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ btn.addEventListener('click',()=>{
     }
 
     let div=document.createElement('div')
+
+    div.innerHTML= `
+<div class="mb-3">
+ <h3>${title}</h3>
+ <textarea class="form-control text " placeholder="Enter Your Work" name="" id="" rows="7">${work}</textarea>
+ <button type = "button" class = "btn del" id = "delete-all-btn">
+ <span><i class = "fas fa-trash"></i></span>
+ Delete All
+</button> 
+</div>
+`
+
+
+
+div.querySelector('.del')
+div.addEventListener('click',()=>{
+ div.remove()
+})
+CreateNotes.append(div)
+  
+  // console.log(work)
+
 })
