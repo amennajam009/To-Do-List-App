fa5ba24f40aee753c684b676f73e7a0fa6509ac2
trench009 <amennajam7@gmail.com>
2022-10-12 01:54:03 +0500

    add

--- index.js
+++ index.js
@@ -3,30 +3,28 @@ let btn=document.querySelector('.btn')
 btn.addEventListener('click',()=>{
   let title=document.querySelector('.title').value;
   let work=document.querySelector('.work').value;
-  let createlist=document.querySelector('. NotesContainer');
+  let createNotes=document.querySelector('. NotesContainer');
    
   if(title===''|| work===''){
     alert('Stupid Fill something');
   }
 
-  let div=document.createElement('.div')
+  let div=document.createElement('div')
    div.classList.add('col-md-4')
   div.innerHTML=`
+  <div class="mb-3">
   <h3>${title}</h3>  
-  <ul>
-  <li>${work}</li>
-  <li>${work}</li>
-  <li>${work}</li>
-  <li>${work}</li>
-  <li>${work}</li>
-  <li>${work}</li>
-  <li>${work}</li>
-  <li>${work}</li>
-  <li>${work}</li>
-  
-  </ul> 
+  <textarea class="form-control text " placeholder="Enter Your Work" name="" id="" rows="7">${work}</textarea>
   <button type = "button" class = "btn del" id = "delete-all-btn">
  <span><i class = "fas fa-trash"></i></span>
  Delete All
-</button>   `
+</button>  
+<div> `
+
+div.querySelector('.del')
+div.addEventListener('click',()=>{
+  div.remove()
+})
+
+createNotes.append(div)
 })
