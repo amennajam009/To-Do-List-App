363fe9ee62b225c3a856005f31634688a0e2465c
trench009 <amennajam7@gmail.com>
2022-10-12 01:45:07 +0500

    add

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ let btn=document.querySelector('.btn')
 btn.addEventListener('click',()=>{
   let title=document.querySelector('.title').value;
   let work=document.querySelector('.work').value;
-  let creatediv=document.querySelector('. NotesContainer');
-
+  let createlist=document.querySelector('. NotesContainer');
+   
   if(title===''|| work===''){
     alert('Stupid Fill something');
   }
 
-  creatediv.innerHTML=`
+  let div=document.createElement('.div')
+   div.classList.add('col-md-4')
+  div.innerHTML=`
   <h3>${title}</h3>  
   <ul>
   <li>${work}</li>
@@ -22,5 +24,9 @@ btn.addEventListener('click',()=>{
   <li>${work}</li>
   <li>${work}</li>
   
-  </ul>    `
+  </ul> 
+  <button type = "button" class = "btn del" id = "delete-all-btn">
+ <span><i class = "fas fa-trash"></i></span>
+ Delete All
+</button>   `
 })
