37e097a83b4e56a1e3e78e22984abc6835d79c69
trench009 <amennajam7@gmail.com>
2022-10-11 19:14:32 +0500

    add

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let btn=document.querySelector('.btn')
 
 btn.addEventListener('click',()=>{
-    let title=document.querySelector('title').value
-    let work=document.querySelector('work').value
+    let title=document.querySelector('.title').value
+    let work=document.querySelector('.work').value
+    let createnotes=querySelector('.NotesContainer');
+    if(title==='' || work===''){
+        alert('Enter the Valid')
+        return;
+    }
 })
